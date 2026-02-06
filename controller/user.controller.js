7fe18675812097ff3fa753b4d615903212088042
jieul2 <jieul2@kakao.com>
2026-02-06 14:09:00 +0900

    로그이 만들기 1~3

---
+++ controller/user.controller.js
@@ -0,0 +1,56 @@
+const User = require("../model/User");
+const bcrypt = require("bcryptjs");
+const saltRounds = 10;
+
+const userController = {};
+
+// 색상 랜덤 생성 함수
+const generatePastelColor = () => {
+  const r = Math.floor(Math.random() * 127 + 128); // 128 ~ 255 사이 (밝은 톤)
+  const g = Math.floor(Math.random() * 127 + 128);
+  const b = Math.floor(Math.random() * 127 + 128);
+
+  return `#${r.toString(16).padStart(2, "0")}${g.toString(16).padStart(2, "0")}${b.toString(16).padStart(2, "0")}`;
+};
+
+userController.createUser = async (req, res) => {
+  try {
+    const { email, name, password, color } = req.body;
+    const user = await User.findOne({ email });
+    if (user) {
+      throw new Error("이미 가입된 유저입니다.");
+    }
+    const salt = bcrypt.genSaltSync(saltRounds);
+    const hash = bcrypt.hashSync(password, salt);
+
+    const userColor = color || generatePastelColor();
+
+    const newUser = new User({ email, name, password: hash, color: userColor });
+    await newUser.save();
+    res.status(200).json({ status: "success" });
+  } catch (err) {
+    res.status(400).json({
+      status: "fail",
+      error: err.message || err,
+    });
+  }
+};
+
+userController.loginWithEmail = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email }, "-createdAt -updatedAt -__v");
+    if (user) {
+      const isMath = bcrypt.compareSync(password, user.password);
+      if (isMath) {
+        const token = user.generateToken();
+        return res.status(200).json({ status: "success", user, token });
+      }
+    }
+    throw new Error("아이디 또는 비밀번호가 일치하지 않습니다.");
+  } catch (err) {
+    res.status(400).json({ status: "fail", error: err.message || err });
+  }
+};
+
+module.exports = userController;
---
+++ model/User.js
@@ -0,0 +1,43 @@
+const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+const Schema = mongoose.Schema;
+const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY;
+
+const userSchema = Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    // User마다 BG-color 바뀔수있게
+    color: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true },
+);
+userSchema.methods.toJSON = function () {
+  const obj = this._doc;
+  delete obj.password;
+  return obj;
+};
+
+userSchema.methods.generateToken = function () {
+  const token = jwt.sign({ _id: this._id }, JWT_SECRET_KEY, {
+    expiresIn: "1d",
+  });
+  return token;
+};
+
+const User = mongoose.model("User", userSchema);
+module.exports = User;
